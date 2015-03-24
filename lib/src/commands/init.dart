1a348b95385ff8b348c4aa478f334cd5e035d187
Andres Araujo <aaraujo@foundationmedicine.com>
2015-03-24 17:37:57 -0600

    clean up code

--- lib/src/commands/init.dart
+++ lib/src/commands/init.dart
@@ -1,7 +1,6 @@
 library ddrive.src.commands.init;
 
 import 'package:unscripted/unscripted.dart';
-import 'package:googleapis/drive/v2.dart' as drive;
 import 'package:path/path.dart' as path;
 
 import '../context.dart' as context;
@@ -11,7 +10,7 @@ class InitCommand {
 
   @SubCommand(help: 'Initialize a Google Drive home directory')
   init({ @Option(help: 'The root directory for Google Drive. Defaults to your current directory') String driveHome }) async {
-    String p = driveHome == null || driveHome.isEmpty ? path.current : driveHome; //path ? path : path.current;
+    String p = driveHome == null || driveHome.isEmpty ? path.current : driveHome;
     bool firstTime = ! await context.credentialsExist(p);
     context.askForAuthorization(p);
   }
